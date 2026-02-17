23487b5ddd69c9fe53836a2340e9b67aa7444d74
Tharushi-Nethmini <it22083296@my.sliit.lk>
2026-02-17 10:04:52 +0530

    update

--- backend/main.py
+++ backend/main.py
@@ -65,7 +65,7 @@ app = FastAPI(title="ScreenPilot Backend", version="0.1")
 # ------------------------------------
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],   # For development
+    allow_origins=["*"],   
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
